0bfef2262613863c7d288f39a84a32c8c1aa070f
SzymonGonerko <szymon.gonerko@gmail.com>
2022-07-14 11:33:05 +0200

    GroupBox

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { UserController } from './components/UserController';
 import { Physics } from '@react-three/cannon';
 import { Plane } from "./components/Plane"
 import { Box } from "./components/Box"
+import { GroupBox } from './components/GroupBox';
 
 import './App.css';
 
@@ -35,31 +36,7 @@ const App : React.FC = () => {
         <ambientLight intensity={1}/>
         <Player/>
 
-        <Box position={[-20, 0, -20]}/>
-        <Box position={[-20, 1, -20]}/>
-        <Box position={[-20, 0, -21]}/>
-        <Box position={[-20, 1, -21]}/>
-        <Box position={[-20, 0, -22]}/>
-        <Box position={[-20, 1, -22]}/>
-        <Box position={[-20, 3, -20]}/>
-        <Box position={[-20, 3, -21]}/>
-        <Box position={[-20, 3, -22]}/>
-
-        <Box position={[0, 0, -30]}/>
-        <Box position={[-1, 0, -30]}/>
-        <Box position={[-2, 0, -30]}/>
-        <Box position={[0, 1, -30]}/>
-        <Box position={[-1, 1, -30]}/>
-        <Box position={[-2, 1, -30]}/>
-        <Box position={[0, 3, -30]}/>
-        <Box position={[-1, 3, -30]}/>
-        <Box position={[-2, 3, -30]}/>
-        <Box position={[-1.5, 5, -30]}/>
-        <Box position={[-0.5, 5, -30]}/>
-        <Box position={[-1, 7, -30]}/>
-
-
-
+        <GroupBox/>
 
         <OrbitControls makeDefault enabled={false} />
         <UserController/>
---
+++ src/components/GroupBox.js
@@ -0,0 +1,28 @@
+import { Box } from "../components/Box"
+
+export const GroupBox = () => (
+    <>
+        <Box position={[-20, 0, -20]}/>
+        <Box position={[-20, 1, -20]}/>
+        <Box position={[-20, 0, -21]}/>
+        <Box position={[-20, 1, -21]}/>
+        <Box position={[-20, 0, -22]}/>
+        <Box position={[-20, 1, -22]}/>
+        <Box position={[-20, 3, -20]}/>
+        <Box position={[-20, 3, -21]}/>
+        <Box position={[-20, 3, -22]}/>
+
+        <Box position={[0, 0, -30]}/>
+        <Box position={[-1, 0, -30]}/>
+        <Box position={[-2, 0, -30]}/>
+        <Box position={[0, 1, -30]}/>
+        <Box position={[-1, 1, -30]}/>
+        <Box position={[-2, 1, -30]}/>
+        <Box position={[0, 3, -30]}/>
+        <Box position={[-1, 3, -30]}/>
+        <Box position={[-2, 3, -30]}/>
+        <Box position={[-1.5, 5, -30]}/>
+        <Box position={[-0.5, 5, -30]}/>
+        <Box position={[-1, 7, -30]}/>
+    </>
+)
