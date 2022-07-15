3ecad12c31ec33214c8c793a036aaa8f7f5b0c26
SzymonGonerko <szymon.gonerko@gmail.com>
2022-07-15 20:00:45 +0200

    Logo

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,10 @@
+import Flow2Code from "../../gltfjsx/Flow2Code"
+
+export const Logo = () => {
+
+return <>
+<group scale={8} position={[-23, 4, 1]} rotation={[0, 1.6, 0]}>
+    <Flow2Code />
+</group>
+</>
+}
